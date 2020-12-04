ba46fb59775f108799fddcbe64fbd31f134fde57
sKyoKun <sk.kyokun@gmail.com>
2020-12-04 10:27:58 +0100

    Day 4 part 1

---
+++ src/Controller/Day4Controller.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Services\CalendarServices;
+use App\Services\InputReader;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
++ @Route("/day4")
+ */
+class Day4Controller extends AbstractController
+{
+    /** @var CalendarServices */
+    private $calendarServices;
+
+    /** @var InputReader */
+    private $inputReader;
+
+    public function __construct(CalendarServices $calendarServices, InputReader $inputReader)
+    {
+        $this->calendarServices = $calendarServices;
+        $this->inputReader = $inputReader;
+    }
+
+    /**
+    + @Route("/")
+     */
+    public function day4()
+    {
+        $validPassports = 0;
+        $inputs = $this->inputReader->getInput('day4.txt');
+        $passports = $this->calendarServices->parseInputsPassports($inputs);
+        foreach ($passports as $passport) {
+            if ($this->calendarServices->isPassportValid($passport)) {
+                $validPassports++;
+            }
+        }
+
+        dump($validPassports);
+        die();
+    }
+
+    /**
+    + @Route("/2")
+     */
+    public function day4Part2()
+    {
+        $validPassports = 0;
+        $inputs = $this->inputReader->getInput('day4.txt');
+        $passports = $this->calendarServices->parseInputsPassports($inputs);
+        foreach ($passports as $passport) {
+            if ($this->calendarServices->isPassportValidWithRestrain($passport)) {
+                $validPassports++;
+            }
+        }
+
+        dump($validPassports);
+        die();
+    }
+
+
+}
--- src/Services/CalendarServices.php
+++ src/Services/CalendarServices.php
@@ -26,4 +26,82 @@ class CalendarServices
 
         return $trees;
     }
+
+    public function parseInputsPassports(array $inputs)
+    {
+        $passports = [];
+        $lastBlankLineValue = 0;
+        foreach ($inputs as $lineNumber => $input) {
+            $previousPassportString = '';
+            if($input == '') {
+                for($i = $lastBlankLineValue; $i < $lineNumber; $i++) {
+                    $previousPassportString .= ' '.$inputs[$i];
+                }
+                $passports[] = $this->parsePassportLine($previousPassportString);
+                $lastBlankLineValue = $lineNumber+1;
+            }
+        }
+
+        // get the last passport as we can't go in the if = ""
+        $lastPassportString = '';
+        for($i = $lastBlankLineValue; $i < count($inputs); $i++) {
+            $lastPassportString .= ' '.$inputs[$i];
+        }
+        $passports[] = $this->parsePassportLine($lastPassportString);
+
+        return $passports;
+    }
+
+    public function isPassportValid(array $passport)
+    {
+        $validKeys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid'];
+
+        if (count($passport) === count($validKeys)) {
+            $arrayDiff = array_diff($validKeys, array_keys($passport));
+            if (empty($arrayDiff)) {
+                return true;
+            }
+        }
+
+        // si j'ai pile 1 de moins et que j'ai pas le cid
+        if ((count($passport) === (count($validKeys) -1)) && (false === array_key_exists('cid', $passport))) {
+            // si j'ai aucune différence sur mon tableau de clé
+            $arrayDiff = array_diff($validKeys, array_keys($passport));
+
+            if (count($arrayDiff) == 1 && 'cid' == $arrayDiff[7]) {
+                return true;
+            }
+
+            return false;
+        }
+
+        return false;
+    }
+
+    public function isPasswordValidWithRestrain(array $passport){
+        if (false === $this->isPassportValid($passport)) {
+            return false;
+        }
+
+        if ((int)$passport['byr'] < 1920 && (int)$passport['byr'] > 2002) {
+            return false;
+        }
+        if ((int)$passport['iyr'] < 2010 && (int)$passport['byr'] > 2020) {
+            return false;
+        }
+        if ((int)$passport['eyr'] < 2020 && (int)$passport['eyr'] > 2030) {
+            return false;
+        }
+    }
+
+    private function parsePassportLine(string $line) {
+        $previousPassportContent = explode(' ', $line);
+        foreach ($previousPassportContent as $keyValuePairs) {
+            if('' !== $keyValuePairs) {
+                $explodedPairs = explode(':', $keyValuePairs);
+                $previousPassport[$explodedPairs[0]] = $explodedPairs[1];
+            }
+        }
+        return $previousPassport;
+    }
 }
